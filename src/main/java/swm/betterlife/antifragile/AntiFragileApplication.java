44b387b73a08a03f68cf51ae3675a8ca1a4cde0d
tjdtn0219 <s_sjang@naver.com>
2024-06-10 22:45:10 +0900

    feat: check style 적용

---
+++ src/main/java/swm/betterlife/antifragile/AntiFragileApplication.java
@@ -0,0 +1,13 @@
+package swm.betterlife.antifragile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFragileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiFragileApplication.class, args);
+    }
+
+}
--- src/test/java/swm/betterlife/antifragile/AntiFragileApplicationTests.java
+++ src/test/java/swm/betterlife/antifragile/AntiFragileApplicationTests.java
@@ -1,10 +1,10 @@
-package swm.betterlife.Antifragile;
+package swm.betterlife.antifragile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AntifragileApplicationTests {
+class AntiFragileApplicationTests {
 
 	@Test
 	void contextLoads() {
