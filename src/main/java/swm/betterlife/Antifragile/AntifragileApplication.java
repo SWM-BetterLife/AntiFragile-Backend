50d2e9501fe9879fb47438cb7df6cbc52b08335e
tjdtn0219 <s_sjang@naver.com>
2024-06-10 14:23:53 +0900

    Spring Init

---
+++ src/main/java/swm/betterlife/Antifragile/AntifragileApplication.java
@@ -0,0 +1,13 @@
+package swm.betterlife.Antifragile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntifragileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntifragileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swm/betterlife/Antifragile/AntifragileApplicationTests.java
@@ -0,0 +1,13 @@
+package swm.betterlife.Antifragile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntifragileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
