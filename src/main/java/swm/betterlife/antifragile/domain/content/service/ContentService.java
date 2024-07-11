d1c3b4224d4ae28c8a4e455adbe7700e7d1d87d0
junseokkim <junsuk8960@naver.com>
2024-07-11 21:35:39 +0900

    SWM-16 feat: 이미 존재하는 콘텐츠에 대한 처리 추가

--- src/main/java/swm/betterlife/antifragile/domain/content/service/ContentService.java
+++ src/main/java/swm/betterlife/antifragile/domain/content/service/ContentService.java
@@ -1,6 +1,11 @@
 package swm.betterlife.antifragile.domain.content.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import swm.betterlife.antifragile.domain.content.dto.response.ContentRecommendResponse;
@@ -17,19 +22,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ContentService {
 
+    private final MongoTemplate mongoTemplate;
     private final ContentRepository contentRepository;
 
     private final DiaryAnalysisService diaryAnalysisService;
 
     @Transactional
     public ContentRecommendResponse saveRecommendContents(String memberId, LocalDate date) {
-        DiaryAnalysis analysis = diaryAnalysisService.getDiaryAnalysisByMemberIdAndDate(memberId, date);
-
+        DiaryAnalysis analysis = diaryAnalysisService
+            .getDiaryAnalysisByMemberIdAndDate(memberId, date);
         List<Content> recommendedContents = getRecommendContentsByAnalysis(analysis);
-        contentRepository.saveAll(recommendedContents);
-        diaryAnalysisService.saveRecommendContents(analysis, recommendedContents);
 
-        return ContentRecommendResponse.from(recommendedContents.stream()
+        List<Content> savedContents = recommendedContents.stream()
+            .map(this::saveOrUpdateContent).toList();
+
+        diaryAnalysisService.saveRecommendContents(analysis, savedContents);
+
+        return ContentRecommendResponse.from(savedContents.stream()
             .map(ContentRecommendResponse.ContentResponse::from).toList());
     }
 
@@ -38,4 +47,23 @@ public class ContentService {
         return Collections.emptyList();
     }
 
+    private Content saveOrUpdateContent(Content recommendedContent) {
+        Query query = new Query(Criteria.where("url").is(recommendedContent.getUrl()));
+        Update update = new Update()
+            .set("title", recommendedContent.getTitle())
+            .set("description", recommendedContent.getDescription())
+            .set("thumbnailImgUrl", recommendedContent.getThumbnailImgUrl())
+            .set("youTubeInfo.subscriberNumber", recommendedContent.getYouTubeInfo().getSubscriberNumber())
+            .set("youTubeInfo.channelName", recommendedContent.getYouTubeInfo().getChannelName())
+            .set("youTubeInfo.channelImg", recommendedContent.getYouTubeInfo().getChannelImg())
+            .set("youTubeInfo.viewNumber", recommendedContent.getYouTubeInfo().getViewNumber())
+            .set("youTubeInfo.likeNumber", recommendedContent.getYouTubeInfo().getLikeNumber());
+
+        return mongoTemplate.findAndModify(
+            query,
+            update,
+            FindAndModifyOptions.options().returnNew(true).upsert(true),
+            Content.class
+        );
+    }
 }
