37e30a34cd82e7a4c8fbf80e80f2e2ac39a83281
junseokkim <junsuk8960@naver.com>
2024-07-11 21:35:16 +0900

    SWM-16 feat: content의 url 인덱싱 설정

--- src/main/java/swm/betterlife/antifragile/common/config/MongoConfig.java
+++ src/main/java/swm/betterlife/antifragile/common/config/MongoConfig.java
@@ -4,12 +4,16 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import swm.betterlife.antifragile.domain.content.entity.Content;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
@@ -41,5 +45,11 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         return new MongoTemplate(
             new SimpleMongoClientDatabaseFactory(mongoClient(), getDatabaseName())
         );
+    }
+
+    @PostConstruct
+    public void initIndexes() {
+        mongoTemplate().indexOps(Content.class)
+            .ensureIndex(new Index().on("url", Sort.Direction.ASC).unique());
     }
 }
