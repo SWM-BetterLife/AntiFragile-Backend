72161685f6b422d7189e11dbd8996bc04063b030
junseokkim <junsuk8960@naver.com>
2024-07-13 01:20:51 +0900

    SWM-16 feat: mongoAuditing 허용

--- src/main/java/swm/betterlife/antifragile/AntiFragileApplication.java
+++ src/main/java/swm/betterlife/antifragile/AntiFragileApplication.java
@@ -3,10 +3,12 @@ package swm.betterlife.antifragile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableMongoRepositories
+@EnableMongoAuditing
 public class AntiFragileApplication {
 
     public static void main(String[] args) {
