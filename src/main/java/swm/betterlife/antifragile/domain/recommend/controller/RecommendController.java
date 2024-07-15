dab3112c8cb7ffb1ae28ae58a1131f6d2bf34de6
송정명 <songjeongmyeong@songjeongmyeong-ui-MacBookPro.local>
2024-07-15 20:41:59 +0900

    SWM-28 feat: OpenAI API 사용 구현

---
+++ src/main/java/swm/betterlife/antifragile/domain/recommend/controller/RecommendController.java
@@ -0,0 +1,27 @@
+package swm.betterlife.antifragile.domain.recommend.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import swm.betterlife.antifragile.common.response.ResponseBody;
+import swm.betterlife.antifragile.domain.recommend.dto.response.OpenAIResponse;
+import swm.betterlife.antifragile.domain.recommend.service.RecommendService;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recommends")
+public class RecommendController {
+    private final RecommendService recommendService;
+
+    @GetMapping("/chatGpt")
+    public ResponseBody<OpenAIResponse> chatGpt(
+        @RequestParam("prompt") String prompt
+    ){
+        return ResponseBody.ok(
+            recommendService.chatGpt(prompt));
+    }
+}
