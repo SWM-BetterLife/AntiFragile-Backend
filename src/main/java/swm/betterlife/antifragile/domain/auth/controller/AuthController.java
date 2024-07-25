02e6d2c648d5b74e6b4771d7c6c095b1244d4a6d
tjdtn0219 <s_sjang@naver.com>
2024-07-26 02:43:50 +0900

    feat: 회원 탈퇴 api 구현

--- src/main/java/swm/betterlife/antifragile/domain/auth/controller/AuthController.java
+++ src/main/java/swm/betterlife/antifragile/domain/auth/controller/AuthController.java
@@ -2,11 +2,14 @@ package swm.betterlife.antifragile.domain.auth.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import swm.betterlife.antifragile.common.response.ResponseBody;
+import swm.betterlife.antifragile.common.security.PrincipalDetails;
 import swm.betterlife.antifragile.domain.auth.dto.request.AuthLoginRequest;
 import swm.betterlife.antifragile.domain.auth.dto.request.AuthLogoutRequest;
 import swm.betterlife.antifragile.domain.auth.dto.request.AuthSignUpRequest;
@@ -40,6 +43,14 @@ public class AuthController {
     ) {
         authService.logout(authLogoutRequest);
         return ResponseBody.ok();
+    }
+
+    @DeleteMapping
+    public ResponseBody<Void> delete(
+        @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        authService.delete(principalDetails.memberId());
+       return ResponseBody.ok();
     }
 
 }
