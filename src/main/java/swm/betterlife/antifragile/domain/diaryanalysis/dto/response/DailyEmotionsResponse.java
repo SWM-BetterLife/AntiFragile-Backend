41e12566275fdcebaa7599a82ce1d96b2a6e1434
송정명 <songjeongmyeong@songjeongmyeong-ui-MacBookPro.local>
2024-07-26 14:38:45 +0900

    SWM-99 refactor: checkstyle 추가

--- src/main/java/swm/betterlife/antifragile/domain/diaryanalysis/dto/response/DailyEmotionsResponse.java
+++ src/main/java/swm/betterlife/antifragile/domain/diaryanalysis/dto/response/DailyEmotionsResponse.java
@@ -4,7 +4,7 @@ import java.util.List;
 import lombok.Builder;
 
 @Builder
-public record DailyEmotionsResponse (
+public record DailyEmotionsResponse(
     List<String> emotions
 ) {
     public static DailyEmotionsResponse from(List<String> emotions) {
